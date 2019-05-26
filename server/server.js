4a33cb6c132f28f67120ec571cb9fbdd0ba78f54
truptigaonkar <trups11.trupti@gmail.com>
2019-05-26 15:24:18 +0200

    Socket connection and check how new user connected and disconnected when page localhost:3000 closed from window

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000
 let app = express();
 
+let server = http.createServer(app);
+
 app.use(express.static(publicPath));
 
-app.listen(port, function(){
+server.listen(port, function(){
     console.log(`Server is running on port ${port}`);
+});
+
+let io = socketIO(server); // get access to socket library http://localhost:3000/socket.io/socket.io.js
+io.on('connection', function(socket){
+    console.log("New user CONNECTED");
+
+    socket.on('disconnect', function(){
+        console.log('User DISCONNECTED')
+    });
 });
