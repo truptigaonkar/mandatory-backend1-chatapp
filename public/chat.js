4323804e387d15b21ab243964f10c313cb858819
truptigaonkar <trups11.trupti@gmail.com>
2019-05-29 10:27:51 +0200

    Created message form and show data on webpage

--- public/chat.js
+++ public/chat.js
@@ -1,14 +1,31 @@
 let socket = io();
 // Connect
-socket.on('connect', function(){
+socket.on('connect', function () {
     console.log('connected to server');
 });
 //Disconnect
-socket.on('disconnect', function(){
+socket.on('disconnect', function () {
     console.log('disconnected from server');
 });
 
 //Server side create message and send to client
-socket.on('newMessage', function(message){
+socket.on('newMessage', function (message) {
     console.log('newMessage', message);
+
+    //Webpage: Created li and append content to body
+    let li = document.createElement('li');
+    li.innerText = `${message.from}: ${message.text}`;
+    document.querySelector('body').appendChild(li);
 });
+
+let messageForm = document.querySelector('#message-form');
+messageForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text:
+            document.querySelector('input[name="message"]').value
+    }, function () {
+
+    })
+})
